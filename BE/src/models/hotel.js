e68bd92d56d97f91cea8a436d325ad631b326c3c
Alone <tranluong460@gmail.com>
2023-07-25 22:42:48 +0700

    feat: fix hotel be

--- BE/src/models/hotel.js
+++ BE/src/models/hotel.js
@@ -1,59 +1,65 @@
 import mongoose from "mongoose";
-const hotelSchema = new mongoose.Schema({
+
+const hotelSchema = new mongoose.Schema(
+  {
     name: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     images: [
-        {
-            status: {
-                type: String,
-                default: "done",
-            },
-            name: {
-                type: String,
-                required: true,
-            },
-            uid: {
-                type: String,
-                required: true,
-            },
-            url: {
-                type: String,
-                required: true,
-            },
+      {
+        status: {
+          type: String,
+          default: "done",
         },
+        name: {
+          type: String,
+          required: true,
+        },
+        uid: {
+          type: String,
+          required: true,
+        },
+        url: {
+          type: String,
+          required: true,
+        },
+      },
     ],
     status: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     address: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     phone: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     email: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     description: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
-    amenities: [{
+    amenities: [
+      {
         code: String,
         value: Boolean,
-    }],
+      },
+    ],
     city: {
-        type: String,
-        required: true,
+      type: String,
+      required: true,
     },
     rating: {
-        type: Number,
-    }
-}, { timestamps: true, versionKey: false })
-export default mongoose.model("Hotel", hotelSchema)
+      type: Number,
+    },
+  },
+  { timestamps: true, versionKey: false }
+);
+export default mongoose.model("Hotel", hotelSchema);
