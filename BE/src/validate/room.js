0d92441c7781e02c46dbe0597e744e1466295d74
Alone <tranluong460@gmail.com>
2023-07-28 23:09:52 +0700

    feat: fix room

--- BE/src/validate/room.js
+++ BE/src/validate/room.js
@@ -2,17 +2,20 @@ import joi from "joi";
 import { errorMessages } from "./component/errorMessages";
 
 const roomValidate = joi.object({
-  name: joi.string().required().messages(errorMessages("Tên")),
+  name: joi.string().required().messages(errorMessages("Tên phòng")),
   image: joi
     .array()
     .min(1)
     .required()
     .items(
       joi.object({
-        status: joi.string().required().messages(errorMessages("Trạng thái")),
-        name: joi.string().required().messages(errorMessages("Tên")),
-        uid: joi.string().required().messages(errorMessages("Uid")),
-        url: joi.string().required().messages(errorMessages("Đường dẫn")),
+        status: joi
+          .string()
+          .required()
+          .messages(errorMessages("Trạng thái ảnh")),
+        name: joi.string().required().messages(errorMessages("Tên ảnh")),
+        uid: joi.string().required().messages(errorMessages("Uid ảnh")),
+        url: joi.string().required().messages(errorMessages("Đường dẫn ảnh")),
       })
     )
     .messages(errorMessages("Ảnh")),
@@ -21,8 +24,8 @@ const roomValidate = joi.object({
   capacity: joi.number().min(0).required().messages(errorMessages("Sức chứa")),
   amenities: joi.string().required().messages(errorMessages("Tiện nghi")),
   description: joi.string().required().messages(errorMessages("Mô tả")),
-  floor: joi.string().required().messages(errorMessages("floor")),
-  view: joi.string().required().messages(errorMessages("View")),
+  floor: joi.string().required().messages(errorMessages("Sàn nhà")),
+  view: joi.string().required().messages(errorMessages("Xem")),
   serviceFree: joi.string().required().messages(errorMessages("Dịch vụ")),
 });
 
