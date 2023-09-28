4f5268ed138b69a3f815b9c93b4d5694257488e1
Alone <tranluong460@gmail.com>
2023-09-28 21:05:54 +0700

    feat: fix hotel detail

--- FE/src/components/client/HotelDetail/InfoHotel.tsx
+++ FE/src/components/client/HotelDetail/InfoHotel.tsx
@@ -6,7 +6,7 @@ import { Button } from "../..";
 const InfoHotel = () => {
   return (
     <>
-      <div className="h-auto md:h-36 w-[78%] absolute bottom-[15px] left-1/2 transform -translate-x-1/2 rounded-lg shadow-md p-0 md:p-5 bg-light dark:bg-dark">
+      <div className="h-auto md:h-36 w-[75%] relative top-[450px] left-1/2 transform -translate-x-1/2 rounded-lg shadow-md p-0 md:p-5 bg-light dark:bg-dark">
         <div className="flex flex-col md:flex-row p-5 lg:p-0 md:p-0">
           <div className="w-full flex flex-col gap-3">
             <h1 className="text-xl lg:text-3xl font-semibold text-textLight dark:text-textDark">
