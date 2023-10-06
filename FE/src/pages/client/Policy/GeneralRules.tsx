799277c4c7d10534facb573915a26813253e4d08
Chi3112 <118526420+Chi3112@users.noreply.github.com>
2023-10-07 01:51:26 +0700

    commit:General Rules resf:DTN-35-FE-General-Rules

--- FE/src/pages/client/Policy/GeneralRules.tsx
+++ FE/src/pages/client/Policy/GeneralRules.tsx
@@ -1,5 +1,168 @@
-const GeneralRules = () => {
-  return <div>Quy định chung</div>;
+import { Container } from "../../../components";
+
+const GeneralTerms = () => {
+  return (
+    <div className="text-justify mb-5">
+      <h1 className="text-2xl font-medium my-5 text-center uppercase">
+        QUY ĐỊNH CHUNG
+      </h1>
+
+      <hr className="border border-divideLight dark:border-divideDark" />
+
+      <Container>
+        
+        <div>
+          <p className="my-[10px]">
+            <strong>
+             I. GIỚI THIỆU
+
+
+            </strong>
+          </p>
+
+          <p>
+            Những quy định sau đây đã được đưa ra bởi: Công ty cổ phần Tập đoàn Mường Thanh, Doanh nghiệp tư nhân xây dựng số 1 tỉnh Điện Biên, Công ty TNHH DV Du lịch Vịnh Hạ Long, Công ty CP Khách sạn Trí Đức, Công ty cổ phần Dầu khí Phương Đông, Công ty cổ phần Thương mại và Xây dựng Hồng Lam, Công ty cổ phần Du lịch Khách sạn Suối Mơ, Công ty CP Golf Xuân Thành, Công ty cổ phần Hoàng gia Cao Bằng (Sau đây gọi chung là “Mường Thanh”) nhằm đảm bảo an toàn, ổn định và bảo vệ các lợi ích của Khách hàng. Các quy định này sẽ được thực hiện nghiêm túc bởi toàn bộ Ban quản lý và nhân viên Khách Sạn mang thương hiệu Mường Thanh. Mường Thanh mong muốn Khách hàng được hưởng thụ các tiện nghi, dịch vụ và quyền lợi của các Khách Sạn. Theo đó, Khách hàng cần hiểu rõ và tuân theo Quy Định Chung (“QĐC”) nhằm đảm bảo Khách nhận được trải nghiệm tốt nhất về Dịch Vụ tại Khách Sạn.
+          </p>
+          <p>
+            QĐC này có thể được sửa đổi, bổ sung tại từng thời điểm theo quyết định của Mường Thanh nhằm nâng cao chất lượng Dịch Vụ và trải nghiệm của Khách hàng và được đăng tải trên Trang Web chính thức của Mường Thanh tại địa chỉ https://booking.muongthanh.com  (Sau đây gọi là “Trang Web”).
+          </p>
+          </div>
+
+        <div>
+          <p className="my-[10px] ">
+            <strong>
+             II. THÔNG TIN VỀ CÁC SẢN PHẨM/ DỊCH VỤ
+            </strong>
+          </p>
+
+          <p>
+           Các sản phẩm/ dịch vụ mà Mường Thanh cung cấp trên Trang Web bao gồm: Dịch vụ lưu trú, tiệc sự kiện hoặc các dịch vụ bổ sung khác mà Mường Thanh cung cấp tại từng thời điểm như vận chuyển, vui chơi giải trí, ẩm thực…
+
+Ngoài ra, các thông tin và tài liệu khác giới thiệu trên trang web này sẽ được Mường Thanh cung cấp một cách hợp pháp và chỉ mang tính chất tham khảo và không nhằm mục đích tư vấn chuyên môn. Vui lòng liên hệ với chúng tôi để nhận được tư vấn về thông tin chi tiết. Đồng thời, Mường Thanh có quyền thay đổi, cập nhật và cải thiện các thông tin, tài liệu đó mà không cần thông báo trước.
+
+Mường Thanh và mọi nhân viên, nhà quản lý, và các bên đại diện hoàn toàn không chịu trách nhiệm gì đối với bất kỳ tổn thất, thiệt hại hoặc chi phí phát sinh do việc truy cập và sử dụng trang web này và các trang web được kết nối với nó, bao gồm nhưng không giới hạn, việc mất đi lợi nhuận, các khoản lỗ trực tiếp hoặc gián tiếp.
+          </p>
+        </div>
+        <div>
+          <p className="my-[10px]">
+            <strong>III. BẢN QUYỀN, THƯƠNG HIỆU</strong>
+          </p>
+
+          <p>
+           Mường Thanh là chủ sở hữu của các nhãn hiệu, biểu tượng và nhãn của các sản phẩm/ dịch vụ được đăng trên trang web này. Mọi cá nhân, tổ chức không được phép sử dụng bất kỳ nhãn hiệu, biểu tượng, nhãn mác nào của sản phẩm, dịch vụ đó khi chưa được Mường Thanh đồng ý bằng văn bản.
+          </p>
+           <p className="my-[10px]">
+            <strong>IV. CÁC TRANG WEB ĐƯỢC KẾT NỐI</strong>
+          </p>
+          <p>Mường Thanh sẽ không chịu trách nhiệm về nội dung hoặc thông tin, tài liệu từ bất kỳ trang web nào khác không thuộc sở hữu/ quản lý của Mường Thanh kết nối với trang web này (“Trang Web khác có kết nối với trang web này”). Khi truy cập và sử dụng các nội dung, thông tin, tài liệu từ các trang web khác có kết nối với trang web này, khách hàng phải chịu mọi rủi ro và tuân theo các điều khoản và điều kiện áp dụng cho việc truy cập/ sử dụng đó.
+
+Chúng tôi không chịu trách nhiệm theo bất kỳ cách nào đối với việc thu thập thông tin cá nhân tại các trang web khác được kết nối với trang web này.</p>
+        </div>
+        <div>
+          <p className="my-[10px]">
+            <strong>V. QUY ĐỊNH CHUNG </strong>
+          </p>
+          <li className="ml-[30px]  my-3  ">
+           Khách hàng xuất trình một trong các giấy tờ pháp lý thể hiện thông tin về số định danh cá nhân theo quy định của pháp luật khi làm thủ tục đăng ký nhận phòng.
+          </li>
+
+          <li className="ml-[30px]  my-3">
+           Khách hàng không được mang vào khách sạn các loại vũ khí, chất phóng xạ, chất có độc tính, các chất gây cháy nổ, vật nuôi, trái cây nặng mùi và những hàng cấm khác theo quy định của pháp luật hoặc của Chính quyền địa phương.
+          </li>
+
+          <li className="ml-[30px] my-3">
+            Nghiêm cấm mọi hình thức cờ bạc, sử dụng ma túy trong khu vực Khách sạn. Trong trường hợp vi phạm thì Khách hàng phải hoàn toàn chịu trách nhiệm trước pháp luật Việt Nam và Khách sạn.
+          </li>
+         
+           <li className="ml-[30px] my-3">
+           Khách hàng không được nấu ăn, không hút thuốc trong phòng ngủ và các khu vực công cộng ngoại trừ khu vực hút thuốc.
+           </li>
+          
+          <li className="ml-[30px] my-3">
+          Thời gian nhận phòng là 14h00 chiều và thời gian trả phòng là 12h00 trưa. Nếu khách hàng nhận phòng sớm hơn hoặc trả phòng muộn hơn thời gian trên thì sẽ phải thanh toán các phụ phí tương ứng theo quy định của Khách sạn.
+           </li>
+
+            <li className="ml-[30px] my-3">
+          Khi nhận phòng khách hàng vui lòng kiểm tra số lượng và tình trạng tài sản hiện có ở trong phòng theo “Danh mục tài sản trong phòng khách”. Nếu không đủ hoặc trong tình trạng hỏng hóc thì quý khách vui lòng báo ngay cho Lễ tân, đồng thời sẽ chịu hoàn toàn trách nhiệm với mất mát, hư hỏng nếu có xảy ra trong quá trình
+lưu trú.
+           </li>
+
+            <li className="ml-[30px] my-3">
+          Tài sản có giá trị như đồ trang sức, tiền và các thiết bị điện tử cá nhân phải được cất giữ trong két sắt tại phòng. Khách sạn không có trách nhiệm pháp lý đối với những mất mát hay hư hại về tài sản cá nhân của Khách hàng trong trường hợp Khách hàng không thực hiện theo quy định.
+           </li>
+
+            <li className="ml-[30px] my-3">
+          Đảm bảo người thân đến thăm khách hàng tại khách sạn phải đăng ký tại quầy Lễ tân và tuân thủ nội quy của khách sạn.
+           </li>
+
+            <li className="ml-[30px] my-3">
+          Khách hàng không sử dụng phòng ngủ cho các mục đích khác như văn phòng
+giao dịch.
+           </li>
+
+            <li className="ml-[30px] my-3">
+          Vì lý do an ninh, khách hàng không được tự ý đổi phòng hoặc di chuyển trang thiết bị, đồ đạc trong phòng sang phòng khác mà không thông báo cho quầy lễ tân.
+           </li>
+
+            <li className="ml-[30px] my-3">
+          Khách hàng thanh toán toàn bộ các hóa đơn và trả chìa khóa phòng cho lễ tân trước khi rời khách sạn.
+           </li>
+
+            <li className="ml-[30px] my-3">
+          Khách hàng tuân thủ các quy định của Nhà nước và của Khách sạn về đảm bảo an toàn phòng, chống dịch bệnh.
+           </li>
+
+            <li className="ml-[30px] my-3">
+          Tuân thủ quy định về an toàn khi đang ở trên xe di chuyển trong khu vực Khách Sạn.
+           </li>
+
+            <li className="ml-[30px] my-3">
+          Cư xử đúng mực tại nơi công cộng: Tôn trọng nguyên tắc đến trước được phục vụ trước; Ưu tiên phụ nữ mang thai, người khuyết tật, người già, yếu; Không gây ồn ào, la hét; Không đặt chân hay gác chân trên ghế và không làm những hành động thiếu văn hóa khác ở những khu vực công cộng; Quản lý trẻ em tại khu vực
+công cộng.
+           </li>
+
+            <li className="ml-[30px] my-3">
+          Khi ra khỏi phòng, Khách cần đảm bảo (i) tất cả các thiết bị điện đã được trả về nguyên trạng; (ii) cửa phòng đã được khóa an toàn; và (iii) không đưa chìa khóa phòng cho người khác.
+           </li>
+
+            <li className="ml-[30px] my-3">
+          Tại các căn hộ khách sạn: tuyệt đối tuân thủ các quy định của Khách Sạn về việc nấu nướng trong phòng, sử dụng các thiết bị điện đúng mục đích. 
+           </li>
+
+            <li className="ml-[30px] my-3">
+          Không mang các loại thức ăn, đồ uống, trái cây, thực phẩm nặng mùi (sầu riêng, mít, các loại mắm…) vào trong phòng Khách Sạn.
+           </li>
+
+            <li className="ml-[30px] my-3">
+          Quý khách vui lòng tuân thủ thời gian hoạt động của hồ bơi tại cơ sở (có thể có thay đổi phụ thuộc vào tính chất của thời tiết, mùa trong năm). Khách hàng sử dụng hồ bơi trước và sau thời gian hoạt động (các cơ sở khác nhau có thể có thời gian đóng cửa/mở cửa khác nhau tùy thuộc vào địa lý/mùa) phải tự đảm bảo và chịu trách nhiệm đối với an toàn sức khỏe, tính mạng của bản thân mà không có bất cứ khiếu nại nào đối với Khách Sạn.
+           </li>
+
+            <li className="ml-[30px] my-3">
+          Khách vui lòng mặc trang phục phù hợp khi ở trong khu vực chung của Khách Sạn, đặc biệt ở những khu vực công cộng như tiền sảnh, nhà hàng…để thể hiện sự tôn trọng đối với các nền văn hoá khác, Khách tuyệt đối không ở trong tình trạng khoả thân, kể cả trẻ em.
+           </li>
+
+
+            <li className="ml-[30px] my-3">
+          Thực hiện các thủ tục khác theo quy định của Mường Thanh và cơ quan chức năng quản lý lưu trú tại địa phương tại từng thời điểm (nếu có).
+           </li>
+
+            <li className="ml-[30px] my-3">
+          Ban quản lý Khách Sạn có thể yêu cầu Khách hàng phải rời Khách Sạn mà không hoàn trả bất cứ chi phí nào đối với những Quý Khách cố tình bỏ qua và vi phạm những quy định và nội quy của Khách Sạn.
+           </li>
+
+            <li className="ml-[30px] my-3">
+          Quy định tiếng ồn: sau 22:00 giờ Khách hàng không được mở tivi, nhạc bằng loa có âm lượng quá lớn hoặc nói chuyện quá lớn để đảm bảo trật tự chung. Tiếng ồn sẽ bị coi là quá mức khi các Khách hàng của Khách sạn cảm thấy phiền hoặc
+khó chịu.
+           </li>
+
+            <li className="ml-[30px] my-3">
+          Thực hiện các thủ tục khác theo quy định của Mường Thanh tại từng thời điểm (nếu có).
+           </li>
+        </div>
+      </Container>
+    </div>
+  );
 };
 
-export default GeneralRules;
+export default GeneralTerms;
+
