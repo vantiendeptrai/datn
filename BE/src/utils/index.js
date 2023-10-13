2529a9635b9da899bee72558eb7142f0bb757227
Alone <tranluong460@gmail.com>
2023-10-14 04:31:41 +0700

    feat: fix

--- BE/src/utils/index.js
+++ BE/src/utils/index.js
@@ -1,16 +1,17 @@
 import { loginToken } from "./authUtils";
 import { sendMailRegister, sendMailOauthRegister } from "./emailUtils";
+import { generateRandomCode } from "./generateRandomUtils";
 import { getGoogleOauthToken, getGoogleUser } from "./oauthUtils";
-import { sendResponse, handleJWTError } from "./responseUtils";
+import { sendResponse } from "./responseUtils";
 import { validationErrors } from "./validationUtils";
 
 export {
   loginToken,
   sendMailRegister,
   sendMailOauthRegister,
+  generateRandomCode,
   getGoogleOauthToken,
   getGoogleUser,
   sendResponse,
-  handleJWTError,
   validationErrors,
 };
--- BE/src/utils/responseUtils.js
+++ BE/src/utils/responseUtils.js
@@ -1,24 +1,6 @@
-import jwt from "jsonwebtoken";
-
 export const sendResponse = (res, status, message, data = null) => {
   return res.status(status).json({
     message: message,
     data: data,
   });
-};
-
-export const handleJWTError = (error, res) => {
-  if (error instanceof jwt.TokenExpiredError) {
-    return res.status(401).json({
-      message: "Token đã hết hạn!",
-    });
-  } else if (error instanceof jwt.NotBeforeError) {
-    return res.status(401).json({
-      message: "Token chưa có hiệu lực!",
-    });
-  } else if (error instanceof jwt.JsonWebTokenError) {
-    return res.status(401).json({
-      message: "Token không hợp lệ!",
-    });
-  }
 };
